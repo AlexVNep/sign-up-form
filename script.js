f5a492a85886c6c212fe40e6b6d4132db88c6a98
Alex <alexvnep@gmail.com>
2024-02-08 10:25:14 +0200

    Change eventlistener to blur, add resetError, showError, isEmpty and confirmPasswordCheck functions to vlaidate the passwords matching

--- script.js
+++ script.js
@@ -2,14 +2,48 @@ const  pwd = document.querySelector('#pwd');
 const conpwd =document.querySelector('#conpwd');
 const p = document.querySelector('.p');
 
-// pwd.addEventListener('change', passwordCheck);
+pwd.addEventListener('blur', passwordCheck);
+conpwd.addEventListener('blur', confirmPasswordCheck);
 
-function passwordCheck() {
-    if (pwd.value != conpwd.value) {
-        let para = document.createElement('p');
-        let node = document.createTextNode('Password does not match!');
-        para.appendChild(node);
-        p.appendChild(para);
+function resetError(element) {
+    p.textContent = ' ';
+    if (element.classList.contains('invalid')) {
+        element.classList.remove('invalid');
     }
 }
 
+function showError(element, message) {
+    //Adds the class invalid to the element showError is being called on
+    element.classList.add('invalid');
+    p.textContent = message;
+}
+
+function isEmpty(string) {
+    return string === ' ';
+}
+
+function passwordCheck() {
+    const password = pwd.value;
+    const confirmPassword = conpwd.value;
+    resetError(pwd);
+    if (!isEmpty(confirmPassword)) {
+        if (password === confirmPassword) {
+            return true;
+        }
+        showError(conpwd, 'Passwords do not match');
+        return false;
+    }
+}
+
+function confirmPasswordCheck () {
+    const password = pwd.value;
+    const confirmPassword = conpwd.value;
+    resetError(conpwd);
+    if (!isEmpty(confirmPassword)) {
+        if (confirmPassword === password) {
+            return true;
+        }
+        showError(pwd, 'Passwords do not match');
+        return false;
+    }
+}
